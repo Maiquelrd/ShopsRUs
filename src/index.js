6bb5ccdbacecb2a9378cc9ccd6faaadf7867f711
Maiquel Mojica <maiquel.mojica@gmail.com>
2021-11-03 13:04:44 -0400

    Arreglo de bug en start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from './app';
 import config from './config'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 async function main(){
     await app.listen(config.port);
